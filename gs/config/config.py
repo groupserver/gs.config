00f01585bc3717de1e696ba43b72656a278c31fc
Michael JasonSmith <mpj17@onlinegroups.net>
2012-10-18 16:50:04 +1100

    Code cleanup, helped by Ninja IDE, and trying to make getInstanceId better.

--- gs/config/config.py
+++ gs/config/config.py
@@ -3,7 +3,6 @@ import os
 from os.path import isfile
 import ConfigParser
 import logging
-import time
 
 try:
     from App.config import getConfiguration
@@ -11,9 +10,10 @@ try:
     USINGZOPE = True
 except ImportError:
     USINGZOPE = False
-    getConfiguration = getRequest = None
+    getConfiguration = getRequest = None  # lint:ok
 
 log = logging.getLogger('gs.config')
+
 
 def bool_(val):
     if val.lower() in ('true', 'yes', 'on'):
@@ -21,24 +21,27 @@ def bool_(val):
     elif val.lower() in ('false', 'no', 'off'):
         val = False
     else:
-        raise ValueError, "Not a bool"
+        raise ValueError("Not a bool")
     return val
+
 
 class ConfigError(Exception):
     pass
 
+
 def getInstanceId():
-    instance_id = 'default'
+    instance_id = ''
     if USINGZOPE:
         request = getRequest()
-        instance_id = 'default'
         if request:
-            instance_id = request.get('HTTP_INSTANCEID', 'default')
+            instance_id = request.get('HTTP_INSTANCEID', '')
+    retval = ((instance_id and instance_id) or 'default')
+    return retval
 
-    return instance_id
 
 class Config(object):
     schema = {}
+
     def __init__(self, configset, configpath=None):
         if USINGZOPE and not configpath:
             # again, try and figure out from our groupserver config,
@@ -52,50 +55,57 @@ class Config(object):
             raise ConfigError('Could not read the configuration, as the '
                 'configuration file "%s" does not exist.' % configpath)
 
-        log.info("Reading the config file <%s>" % configpath) 
+        log.info("Reading the config file <%s>" % configpath)
         self.parser = ConfigParser.SafeConfigParser()
         self.parser.read(configpath)
 
-        if not self.parser.has_section('config-'+configset):
-            raise ConfigError('No configuration set "%s" defined in file.' % configset)
+        if not self.parser.has_section('config-' + configset):
+            m = 'No configuration set "%s" defined in file.' % configset
+            raise ConfigError(m)
         self.configset = configset
 
     def set_schema(self, configtype, schema):
-        assert isinstance(schema, dict), "Schema must be a dictionary of converters"
+        assert isinstance(schema, dict),\
+            "Schema must be a dictionary of converters"
         self.schema[configtype] = schema
 
     def get_schema(self, configtype):
-        if not self.schema.has_key(configtype):
-            raise ConfigError('No schema defined for configuration type "%s".' % configtype)
+        if not (configtype in self.schema):
+            m = 'No schema defined for configuration type "%s".' % configtype
+            raise ConfigError(m)
 
         return self.schema[configtype]
-    
+
     def keys(self):
-        return self.parser.options('config-'+self.configset)
+        return self.parser.options('config-' + self.configset)
 
     def get(self, configtype):
         if configtype not in self.keys():
-            raise ConfigError('No configuration defined for "%s" in set "%s".' %
-                      (configtype, self.configset))
+            m = 'No configuration defined for "%s" in set "%s".' %\
+                  (configtype, self.configset)
+            raise ConfigError(m)
 
-        secval = self.parser.get('config-'+self.configset, configtype)
-        if not self.parser.has_section(configtype+'-'+secval):
-            raise ConfigError('No configuration for type "%s" with value "%s".' % 
-                               (configtype, secval))
-       
+        secval = self.parser.get('config-' + self.configset, configtype)
+        if not self.parser.has_section(configtype + '-' + secval):
+            m = 'No configuration for type "%s" with value "%s".' %\
+                   (configtype, secval)
+            raise ConfigError(m)
+
         schema = self.get_schema(configtype)
 
         retval = {}
-        for option in self.parser.options(configtype+'-'+secval):
+        for option in self.parser.options(configtype + '-' + secval):
             if option not in schema:
-                raise ConfigError('No option "%s" defined in schema for "%s"' %
-                          (option, configtype))
-            val = self.parser.get(configtype+'-'+secval, option)
+                m = 'No option "%s" defined in schema for "%s"' %\
+                          (option, configtype)
+                raise ConfigError(m)
+            val = self.parser.get(configtype + '-' + secval, option)
             try:
                 val = schema[option](val)
             except:
-                raise ConfigError('Unable to convert option "%s" value "%s" using "%s"' % 
-                                  (option, val, schema[option]))
+                m = 'Unable to convert option "%s" value "%s" using "%s"' %\
+                      (option, val, schema[option])
+                raise ConfigError(m)
             retval[option] = val
 
         return retval
