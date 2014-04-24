b18f1b562aeb4db7ba33d3d39e7cddfd5759ab3e
Michael JasonSmith <mpj17@onlinegroups.net>
2014-04-24 11:29:31 -0700

    Updating the init.

--- gs/config/__init__.py
+++ gs/config/__init__.py
@@ -2,4 +2,7 @@
 from __future__ import absolute_import
 #lint:disable
 from .config import Config, getInstanceId
+from .errors import (ConfigError, ConfigPathError, ConfigFileError,
+    ConfigSetError, ConfigNoSchemaError, ConfigNoOptionError,
+    ConfigNoSectionError, ConfigConvertError)
 #lint:enable
