a649c7e6ad8132dcc479348a5702dba2a25e789e
Michael JasonSmith <mpj17@onlinegroups.net>
2014-04-24 09:48:31 -0700

    Adding unit tests for gs.config.config.getInstanceId

--- gs/config/tests/config.py
+++ gs/config/tests/config.py
@@ -14,11 +14,12 @@
 ##############################################################################
 from __future__ import absolute_import, unicode_literals
 from unittest import TestCase
+from mock import MagicMock
 import gs.config.config
 
 
 class TestBool(TestCase):
-    '''Test the gs.config.config module'''
+    '''Test the gs.config.config.bool_ function'''
 
     def test_bool_true(self):
         vals = ('true', 'yes', 'on')
@@ -33,4 +34,50 @@ class TestBool(TestCase):
             self.assertFalse(r)
 
     def test_bool_garbage(self):
-        self.assertRaises(ValueError, gs.config.config.bool_, 'I am a fish.')
+        self.assertRaises(ValueError, gs.config.config.bool_, 'Ethyl the frog.')
+
+
+class FakeRequest(object):
+    '''A fake request-object, for testing gs.config.config.getInstanceId.'''
+
+    def get(self, header, default):
+        return 'parana'
+
+
+class FakeRequestDef(object):
+    '''A fake request-object, for testing gs.config.config.getInstanceId,
+    which returns the default for the get-method.'''
+
+    def get(self, header, default):
+        return default
+
+
+class TestGetInstanceId(TestCase):
+    '''Test the gs.config.config.getInstance function'''
+
+    def setUp(self):
+        req = FakeRequest()
+        gs.config.config.getRequest = MagicMock(return_value=req)
+        self.oldUsingZope = gs.config.config.USINGZOPE
+
+    def tearDown(self):
+        gs.config.config.USINGZOPE = self.oldUsingZope
+
+    def test_getInstance_no_zope(self):
+        gs.config.config.USINGZOPE = False
+        r = gs.config.config.getInstanceId()
+        self.assertEqual('default', r)
+
+    def test_getInstance_zope(self):
+        gs.config.config.USINGZOPE = True
+        r = gs.config.config.getInstanceId()
+        self.assertEqual(1, gs.config.config.getRequest.call_count)
+        self.assertEqual('parana', r)
+
+    def test_getInstance_zope_no_header(self):
+        gs.config.config.USINGZOPE = True
+        req = FakeRequestDef()
+        gs.config.config.getRequest = MagicMock(return_value=req)
+        r = gs.config.config.getInstanceId()
+        self.assertEqual(1, gs.config.config.getRequest.call_count)
+        self.assertEqual('default', r)
