2ca217a14f69d4a23c60341f03aed3732409d2e1
Michael JasonSmith <mpj17@onlinegroups.net>
2014-04-25 14:29:48 -0700

    Updated metadata

--- setup.py
+++ setup.py
@@ -12,17 +12,22 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
+import codecs
 import os
 from setuptools import setup, find_packages
 from version import get_version
 
 version = get_version()
 
+with codecs.open('README.txt', encoding='utf-8') as f:
+    long_description = f.read()
+with codecs.open(os.path.join("docs", "HISTORY.txt"), encoding='utf-8') as f:
+    long_description += '\n' + f.read()
+
 setup(name='gs.config',
     version=version,
     description="Core GS config.",
-    long_description=open("README.txt").read() + "\n" +
-                      open(os.path.join("docs", "HISTORY.txt")).read(),
+    long_description=long_description,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         "Environment :: Web Environment",
@@ -44,6 +49,8 @@ setup(name='gs.config',
     keywords='groupserver config configuration',
     author='Richard Waid',
     author_email='richard@onlinegroups.net',
+    maintainer='Michael JasonSmith',
+    maintainer_email='mpj17@onlinegroups.net',
     url='https://source.iopen.net/groupserver/gs.config/',
     license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
