5c8176c6b6d1480a6f695c2f8bbd9701c2a5d95a
Michael JasonSmith <mpj17@onlinegroups.net>
2014-05-05 14:49:41 -0700

    Switching to Sphinx documentation.

--- README.txt
+++ README.txt
@@ -45,53 +45,8 @@ file_ of a particular structure.
 ================
 
 The ``gs.config.Config`` class that represents the system
-configuration.
-
-``Config(setId, [fileName])``:
-  Create a new configuration class for the set with the ID ``setId``. If
-  ``fileName`` is provided then the configuration will be read from that
-  file, otherwise it will be read from the default (``etc/gsconfig.ini``)
-
-``Config.set_schema(sectionId, optionsDict)``:
-  Set the schema that is used for parsing the options from the section with
-  the identifier ``sectionId``. The ``optionsDict`` is a dictionary of
-  ``optionId: type`` pairs. The section for the set specified earlier will
-  be used.
-
-``Config.get(optionId)``:
-  Get the option with the identifier ``optionId``. The value will be cast
-  to using the type specified by ``set_schema``.
-
-Example
--------
-
-A configuration class is initialised. The second parameter is optional,
-depending on the degree to which we want the environment to configure things
-automatically::
-
- >>> from gs.config import Config
- >>> config = Config('default', '/example/config/file.ini')
-
-A schema must be provided before data is retrieved. For example,
-setting the server to be a string, and a port to be an integer::
-
- >>> config.set_schema('smtp', {'server': str, 'port': int})
-
-Then a specific configuration section, with all the options can
-be retrieved as a dict::
-
- >>> config.get('smtp')
- {'port': 2525, 'server': localhost}
-
-If file fails to fit the schema then a ConfigError is raised::
-
- >>> c.set_schema('wibble', {'someparam': int})
- >>> c.get('wibble')
- Traceback (most recent call last):
-   File "<console>", line 1, in <module>
-   File "/home/deploy/groupserver-12.05/src/gs.config/gs/config/config.py", line 70, in get
-     (option, val, schema[option]))
- ConfigError: Unable to convert option "someparam" value "one" using "<type 'int'>"
+configuration. See the Sphinx documentation in this module for
+more information.
 
 File
 ====
--- gs/config/config.py
+++ gs/config/config.py
@@ -47,6 +47,20 @@ def bool_(val):
 
 
 def getInstanceId():
+    '''Get the ID of the current instance.
+
+:returns: The ID the of the instance, or ``default``
+:rtype: ``str``
+
+It can be useful to have multiple *instances* running on one server, but
+configured seperately. The ``getInstanceId`` function gets the ID of the
+current instance by looking it up in the ``HTTP_INSTANCEID`` property of
+the current Zope HTTP request. If Zope (and HTTP) are not being used
+then ``default`` is returned.
+
+This function is defined in :mod:`gs.config` mostly out of convinience, as
+GroupServer normally organises its configuration into *instances.*
+'''
     instance_id = ''
     if USINGZOPE:
         request = getRequest()
@@ -57,9 +71,25 @@ def getInstanceId():
 
 
 class Config(object):
+    '''The configuration.
+
+:method: Config(configset, configpath=None)
+
+:param str configset: The name of the configuration set to read.
+:param str configpath: The path to the configration file. If ``None`` *and*
+                       Zope is being used then ``etc/gsconfig.ini`` is read
+                       from instance directory.
+:raises ConfigPathError: No path could be found.
+:raises ConfigFileError: The configration file could not be read.
+:raises ConfigSetError: The configuration file does not contain ``configset``.
+
+The actual parsing of the configuration file is done by the :mod:`ConfigParser`
+module.
+'''
     schema = {}
 
     def __init__(self, configset, configpath=None):
+        '''Initialise the configuration'''
         if USINGZOPE and not configpath:
             # again, try and figure out from our groupserver config,
             # otherwise abort.
@@ -86,6 +116,20 @@ class Config(object):
         self.configset = configset
 
     def set_schema(self, configtype, schema):
+        '''Set the schema that is used for parsing the options.
+
+:param str configtype: The identifier for the section of the configration.
+:param dict schema: The schema for the section, as ``optionId: type`` pars.
+
+When the value for an option in a section is retrieved its *type* is coerced
+from a string to one of the types passed in as :param:`schema`.
+
+**Example**::
+
+    s = {'station': str,
+         'frequency': int,}
+    conf.set_schema('radio', s)
+'''
         if not isinstance(schema, dict):
             m = "Schema must be a dictionary of converters, not a {0}"
             msg = m.format(type(schema))
@@ -93,6 +137,13 @@ class Config(object):
         self.schema[configtype] = schema
 
     def get_schema(self, configtype):
+        '''Get the schema that is currently set for a section.
+
+:param str confgitype: The identifier for the section.
+:returns: The schema that is currently set for the section.
+:rtype: A ``dict``, containing ``optionId: type`` pairs.
+:raises ConfigNoSchemaError: No schema with the identifer ``configtype`` found.
+'''
         if configtype not in self.schema:
             m = 'No schema defined for configuration type "{0}".'
             msg = m.format(configtype)
@@ -100,9 +151,27 @@ class Config(object):
         return self.schema[configtype]
 
     def keys(self):
+        '''Get the list of sections that are currently defined.
+
+:returns: A list of sections for the configuration set.
+:rtype: ``list``'''
         return self.parser.options('config-' + self.configset)
 
     def get(self, configtype):
+        '''Get the values defined in a section
+
+:param str configtype: The ID for the section to retrieve.
+:returns: The values for all options in the provided section/
+:rtype: A ``dict`` containing ``optionId: value`` pairs. The values are
+        coerced using the schema set by :meth:`set_schema`.
+:raises ConfigNoSectionError: No section for the ID in ``configtype`` exists.
+:raises ConfigNoOption: An option could not be found in the section.
+:raises ConfigConvertError: An option could not be coerced.
+
+**Example**::
+
+    smtpConfig = config.get('smtp')
+'''
         if configtype not in self.keys():
             m = 'No configuration defined for "{0}" in set "{1}".'
             msg = m.format(configtype, self.configset)
